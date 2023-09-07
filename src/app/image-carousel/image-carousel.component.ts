bc1e0f107a75aecc042f3d2914fa3dd0f1d9cab4
alexisgerman-dcsg <143202938+alexisgerman-dcsg@users.noreply.github.com>
2023-09-06 23:50:19 -0600

    2 files modified, 4 added

---
+++ src/app/image-carousel/image-carousel.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'imagecarousel',
+  exportAs: 'imagecarousel',
+  templateUrl: './image-carousel.component.html',
+  styleUrls: ['./image-carousel.component.scss'],
+})
+export class ImageCarousel {
+  items = [{ title: 'Slide 1' }, { title: 'Slide 2' }, { title: 'Slide 3' }];
+  
+  scrollList(direction: 'left' | 'right'): void {
+    const container = document.querySelector('.scrollable-list');
+    const scrollAmount = 200; // Puedes ajustar esta cantidad según tus necesidades
+
+    if (container) {
+      if (direction === 'left') {
+        container.scrollLeft -= scrollAmount;
+      } else {
+        container.scrollLeft += scrollAmount;
+      }
+    }
+  }
+}

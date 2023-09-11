15860db1f19e60043de8e6d0f32ed33023bd6827
alexisgerman-dcsg <143202938+alexisgerman-dcsg@users.noreply.github.com>
2023-09-11 00:09:30 -0600

    1 file modified

--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -8,34 +8,32 @@ import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angu
   styleUrls: ['./image-carousel.component.scss'],
 })
 export class ImageCarousel {
-  images = [700, 800, 807].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = ['https://photos-uat-us.bazaarvoice.com/photo/2/cGhvdG86Y29uY2llcmdlYXBpZG9jdW1lbnRhdGlvbg/f918b4d7-4a7a-530a-8110-a79b23718ee0', 'https://photos-uat-us.bazaarvoice.com/photo/2/cGhvdG86Y29uY2llcmdlYXBpZG9jdW1lbnRhdGlvbg/be9256eb-24d7-5b03-b0ec-5c0b9232b3b5', 'https://photos-uat-us.bazaarvoice.com/photo/2/cGhvdG86Y29uY2llcmdlYXBpZG9jdW1lbnRhdGlvbg/f918b4d7-4a7a-530a-8110-a79b23718ee0', 'https://photos-uat-us.bazaarvoice.com/photo/2/cGhvdG86Y29uY2llcmdlYXBpZG9jdW1lbnRhdGlvbg/f918b4d7-4a7a-530a-8110-a79b23718ee0', 'https://photos-uat-us.bazaarvoice.com/photo/2/cGhvdG86Y29uY2llcmdlYXBpZG9jdW1lbnRhdGlvbg/f918b4d7-4a7a-530a-8110-a79b23718ee0']
   
   buttonLeft: boolean = true;
   buttonRight: boolean = false;
   @ViewChild('scrollableList') scrollableList!: ElementRef;
-
+  
   constructor() {}
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event): void {
-    // Aquí puedes realizar acciones cuando cambie el tamaño de la pantalla
-    // event.target contiene la ventana (window) que cambió de tamaño
-    console.log('La pantalla cambió de tamaño.');
-    console.log(this.isHorizontalScrollAvailable())
-
+    this.verificarScrollHorizontal();
   }
 
   ngAfterViewInit() {
-    // Establece el HostListener en el elemento con la ID #scrollableList
     this.scrollableList.nativeElement.addEventListener('scroll', this.onScroll.bind(this));
-    setTimeout(() => {
-      console.log(this.isHorizontalScrollAvailable())
-      if (this.isHorizontalScrollAvailable()) {
-        this.buttonLeft = true;
-        this.buttonRight = true;
-      }
-    });
-    
+  }
+
+  verificarScrollHorizontal() {
+    const elemento = this.scrollableList.nativeElement;
+    if (elemento.scrollWidth > elemento.clientWidth) {
+      this.buttonLeft = true;
+      this.buttonRight = true;
+    } else {
+      this.buttonLeft = true;
+      this.buttonRight = true;
+    }
   }
 
   onScroll(event: Event) {
@@ -45,7 +43,6 @@ export class ImageCarousel {
     if (scrollableList.scrollLeft === 0) {
       this.buttonLeft = true;
       this.buttonRight = false;
-      console.log('Estás en la posición inicial.');
       // Puedes ejecutar acciones específicas aquí
     }
 
@@ -68,18 +65,6 @@ export class ImageCarousel {
       } else {
         container.scrollLeft += scrollAmount;
       }
-    }
-  }
-
-  isHorizontalScrollAvailable() {
-    const element = document.getElementById('scrollableList');
-    console.log(element);
-    if (element) {
-      // Verificar si el scroll horizontal está disponible
-      return element.scrollWidth > element.clientWidth;
-    } else {
-      // El elemento con el ID especificado no se encontró en el DOM
-      return false;
     }
   }
 
